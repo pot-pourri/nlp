03af8e63d91c9b0a0f4c3a23c73d762b1daf0cb6
aureooms <aurelien.ooms@gmail.com>
2015-01-23 10:15:37 +0100

    fix #2

---
+++ test/js/src/stemming/porter.js
@@ -0,0 +1,52 @@
+
+var t = function ( source , expected ) {
+
+	var p , pi , pj , oj , output ;
+
+	p = source.split( "" ) ;
+	pi = 0 ;
+	pj = p.length ;
+
+	oj = nlp.porter( p , pi , pj ) + 1 ;
+
+	output = p.slice( pi , oj ).join( "" ) ;
+
+	deepEqual( output , expected , source ) ;
+
+} ;
+
+test( "Porter stemmer" , function ( ) {
+
+	t( "ion"       , "ion" ) ;
+
+	t( "ied"       , "i" ) ;
+	t( "aing"      , "a" ) ;
+
+	t( "caresses"  , "caress" ) ;
+	t( "ponies"    , "poni" ) ;
+	t( "ties"      , "ti" ) ;
+	t( "caress"    , "caress" ) ;
+	t( "cats"      , "cat" ) ;
+
+	t( "feed"      , "feed" ) ;
+	t( "agreed"    , "agre" ) ;
+	t( "disabled"  , "disabl" ) ;
+
+	t( "matting"   , "mat" ) ;
+	t( "mating"    , "mate" ) ;
+	t( "meeting"   , "meet" ) ;
+	t( "milling"   , "mill" ) ;
+	t( "messing"   , "mess" ) ;
+
+	t( "meetings"  , "meet" ) ;
+
+	t( "journalism" , "journal" ) ;
+
+	t( "fossilization" , "fossil" ) ;
+	t( "civilization" , "civil" ) ;
+
+	reference = require( "../../data/stemming/porter/reference.js" ) ;
+
+	reference.pairs.forEach( t.apply.bind( t , null ) ) ;
+
+} ) ;
