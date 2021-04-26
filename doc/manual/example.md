a7f8eb7d94757ac06ad2ca8005292c9c840b0ac1
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-26 16:08:00 +0200

    :unamused: chore: Fix broken links, badges, and dependencies.

--- doc/manual/example.md
+++ doc/manual/example.md
@@ -1,3 +1,3 @@
 # Examples
 
-> More examples in [the test files](https://github.com/aureooms/js-nlp/tree/main/test/src).
+> More examples in [the test files](https://github.com/make-github-pseudonymous-again/js-nlp/tree/main/test/src).
--- doc/scripts/header.js
+++ doc/scripts/header.js
@@ -17,7 +17,7 @@ domReady(() => {
 	header.insertBefore(projectname, header.firstChild);
 
 	const testlink = document.querySelector('header > a[data-ice="testLink"]');
-	testlink.href = 'https://coveralls.io/github/aureooms/js-nlp';
+	testlink.href = 'https://coveralls.io/github/make-github-pseudonymous-again/js-nlp';
 	testlink.target = '_BLANK';
 
 	const searchBox = document.querySelector('.search-box');
