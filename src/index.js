5c7895f1dee1d12b472007f39d5f41ade131b43b
make-github-pseudonymous-again <5165674+make-github-pseudonymous-again@users.noreply.github.com>
2021-06-15 14:52:57 +0200

    :broom: cleaning: Remove extracted code.
    BREAKING CHANGE: This removes some functionality.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export * from './compare/index.js';
 export * from './distance/index.js';
-export * from './matching/index.js';
 export * from './ngrams/index.js';
 export * from './stemming/index.js';
--- src/matching/index.js
+++
@@ -1,2 +0,0 @@
-export * from './find.js';
-export * from './rabinkarp.js';
